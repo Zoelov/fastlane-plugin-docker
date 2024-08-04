ce4c1800dfe710203873bc728807745dc62d1ba1
haowang <haowang@deepglint.com>
2024-08-04 23:36:07 +0800

    fix: build arg

--- lib/fastlane/plugin/docker/actions/docker_client.rb
+++ lib/fastlane/plugin/docker/actions/docker_client.rb
@@ -2,7 +2,7 @@ module Fastlane
   module Actions
     class DockerClient
       def build(repository, path, dockerfile, changelog, tag: "latest")
-        result = Actions.sh "docker build  ----build-arg changelog=#{changelog.shellescape} -f #{dockerfile.shellescape} --pull -t #{repository.shellescape}:#{tag.shellescape} #{path.shellescape}"
+        result = Actions.sh "docker build --build-arg changelog=#{changelog.shellescape} -f #{dockerfile.shellescape} --pull -t #{repository.shellescape}:#{tag.shellescape} #{path.shellescape}"
 
         # Image ID is the last word of the last line
         return result.lines.last.split(" ").last
